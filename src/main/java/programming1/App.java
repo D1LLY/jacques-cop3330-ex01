2241c49f9a978615909481e0b671cea254be0ea9
dilly <dillywoodjean@gmail.com>
2021-06-03 18:57:04 -0400

    ooooo

---
+++ src/main/java/programming1/App.java
@@ -0,0 +1,23 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Dilly Jacques
+ */
+
+package programming1;
+
+import java.util.Scanner;
+
+
+public class App {
+    public static void main(String[] args) {
+        Scanner in =new Scanner(System.in);
+
+        System.out.print("What is your name? ");
+        String name= in.nextLine();
+
+        String outputString= "Hello, " + name + ", nice to meet you!";
+
+        System.out.println(outputString);
+
+    }
+}
